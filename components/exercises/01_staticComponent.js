ff558e96b3a1f8b8eed1de5c843b29bfd027180d
Huayu Qin <445670669@qq.com>
2020-10-25 15:41:38 +0000

    Exercise 1 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -3,7 +3,30 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>HDip Computer Science - Modules table</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>WEB</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>Agile</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
